c3ed78598ac71a4c3d8b319b9c06e0576846c808
Gustavo Souza <gustavogss.dev@gmail.com>
2023-02-04 17:57:13 -0300

    Adiconando dados

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -1,5 +1,11 @@
 import logo from "../../assets/logo.png";
 
+import tomate from '../../assets/frutas/Tomate.png';
+import brocolis from '../../assets/frutas/Brócolis.png';
+import batata from '../../assets/frutas/Batata.png';
+import pepino from '../../assets/frutas/Pepino.png';
+import abobora from '../../assets/frutas/Abóbora.png';
+
 const cesta = {
     header: {
         titulo: "Detalhes da Cesta",
@@ -10,7 +16,32 @@ const cesta = {
         nameFarm: "Hort Fruit Farm",
         description: "Uma cesta de produtos orgânicos selecionados diretamente da fazenda para sua cozinha",
         price: "R$ 40,00"
-    }
+    },
+    itens: {
+        titulo: "Itens da cesta",
+        lista: [
+          {
+            nome: "Tomate",
+            imagem: tomate,
+          },
+          {
+            nome: "Brócolis",
+            imagem: brocolis,
+          },
+          {
+            nome: "Batata",
+            imagem: batata,
+          },
+          {
+            nome: "Pepino",
+            imagem: pepino,
+          },
+          {
+            nome: "Abóbora",
+            imagem: abobora,
+          }
+        ]
+      }
 }
 
 export default cesta;
